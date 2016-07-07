4bd60e0500c8b36cd8aed9bbd9f597e0c15e9b02
wangjihuiupc <wangjihuiupc@163.com>
2016-07-08 00:20:20 +0800

    deal conflict

---
+++ src/main/java/org/wjh/solar/constant/MemcachedConstant.java
@@ -0,0 +1,18 @@
+package org.wjh.solar.constant;
+
+/**
+ * memcached 常量
+ * 
+ * @author wangjihui
+ *
+ */
+public class MemcachedConstant {
+    public static final int EXPIRE_10_MINUTES = 10 * 60;
+    public static final int EXPIRE_30_MINUTES = 30 * 60;
+    public static final int EXPIRE_1_HOUR = 60 * 60;
+    public static final int EXPIRE_12_HOURS = 10 * 60 * 60;
+    public static final int EXPIRE_1_DAY = 24 * 60 * 60;
+    public static final int EXPIRE_10_DAYS = 10 * 24 * 60 * 60;
+    public static final int EXPIRE_1_MONTH = 30 * 24 * 60 * 60;
+    public static final int EXPIRE_6_MONTHS = 6 * 30 * 24 * 60 * 60;
+}
