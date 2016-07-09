8558c7d661fe45233abdf06fdee7e207802d8f50
wangjihuiupc <wangjihuiupc@163.com>
2016-07-09 16:16:46 +0800

    mod:添加修改

---
+++ src/main/java/org/wjh/solar/dao/UserDao.java
@@ -0,0 +1,16 @@
+package org.wjh.solar.dao;
+
+import org.wjh.solar.domain.User;
+
+/**
+ * 用户表接口
+ * 
+ * @author wangjihui
+ *
+ */
+public interface UserDao extends BaseDao<User> {
+
+    public User getByUserId(String userId);
+
+    public void deleteByUserId(String userId);
+}
