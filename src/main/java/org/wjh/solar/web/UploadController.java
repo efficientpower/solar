b0effc614b5f120ffe5f536e72821a76ecebfaae
wangjihuiupc <wangjihuiupc@163.com>
2016-08-29 23:33:30 +0800

    mod：fileupload

--- src/main/java/org/wjh/solar/web/UploadController.java
+++ src/main/java/org/wjh/solar/web/UploadController.java
@@ -1,31 +1,41 @@
 package org.wjh.solar.web;
 
 import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.IOException;
 import java.io.OutputStream;
 import java.io.RandomAccessFile;
 import java.util.Map;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.FileItemFactory;
+import org.apache.commons.fileupload.FileItemIterator;
+import org.apache.commons.fileupload.FileItemStream;
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.multipart.MultipartFile;
 import org.wjh.solar.po.AjaxResult;
 import org.wjh.solar.po.AjaxResult.MobileCode;
 import org.wjh.solar.utils.RegExpUtils;
 
 @Controller
 public class UploadController {
+    public static final String TMP_FILE_DIR = "/home/wangjihui/tmp";
+    public static final String FILE_FIELD_NAME = "fileData";
     private static final int BUFFFER_SIZE = 2*1024*1024;
-    private static final int LIMIT_SIZE = 2000*1024*1024;
     private static Log logger = LogFactory.getLog(UploadController.class);
 
     @RequestMapping("/solar/upload.do")
     @ResponseBody
     public AjaxResult<Map<String, Object>> upload(
-            @RequestParam("fileData") MultipartFile uploadFile,
+            HttpServletRequest request,
             @RequestParam("md5") String md5, OutputStream os) {
         AjaxResult<Map<String, Object>> res = new AjaxResult<Map<String, Object>>();
         try {
@@ -36,25 +46,19 @@ public class UploadController {
                  res.setMessage(MobileCode.PARAM_EXCEPTION.getMessage());
                  return res;
              }
-             if(uploadFile.getSize() > LIMIT_SIZE){
-                 logger.error("file size is too large!");
-                 res.setFlag(MobileCode.FAIL.intValue());
-                 res.setMessage("文件超过" + LIMIT_SIZE);
-                 return res;
-             }
-             String file = "/home/wangjihui/MSHHDD";
-             BufferedInputStream buffIns = new BufferedInputStream(uploadFile.getInputStream(),BUFFFER_SIZE);
-             RandomAccessFile rdf = new RandomAccessFile(file, "rw");
-             try{
-                 rdf.seek(rdf.length()); 
-                 byte buf[] = new byte[BUFFFER_SIZE];
-                 int length;
-                 while((length= buffIns.read(buf)) > -1){
-                     rdf.write(buf,0,length);
+             String fileName = "/home/wangjihui/workspace/tmp/" + md5;
+             long readBytes = 0;
+             long totalSize = 0;
+             FileItemFactory factory = new DiskFileItemFactory(DiskFileItemFactory.DEFAULT_SIZE_THRESHOLD, new File("/home/wangjihui/workspace/tmp"));  
+             ServletFileUpload upload = new ServletFileUpload(factory);
+             FileItemIterator iterator = upload.getItemIterator(request);
+             while(iterator.hasNext()){
+                 FileItemStream itemStream = iterator.next();
+                 if(FILE_FIELD_NAME.equals(itemStream.getFieldName())){
+                     readBytes = writeRdfFile(new File(fileName).length(), itemStream, fileName);
+                     FileItem item = factory.createItem(itemStream.getFieldName(), itemStream.getContentType(), itemStream.isFormField(), itemStream.getName());
+                     totalSize = item.getSize();
                  }
-             }finally{
-                 buffIns.close();
-                 rdf.close();
              }
         } catch (Exception e) {
             logger.error("upload failed! ", e);
@@ -62,5 +66,38 @@ public class UploadController {
             res.setMessage(MobileCode.FAIL.getMessage());
         }
         return res;
+    }
+    
+    private long writeRdfFile(long readBytes,FileItemStream itemStream, String filePath){
+        BufferedInputStream buffIns = null;
+        RandomAccessFile rdf  = null;
+        try {
+            buffIns = new BufferedInputStream(itemStream.openStream());
+            buffIns.skip(readBytes);
+            rdf = new RandomAccessFile(new File(filePath), "rw");
+            rdf.seek(rdf.length());
+            int length ;
+            byte buffer [] = new byte[BUFFFER_SIZE];
+            while((length = buffIns.read(buffer)) > -1){
+                rdf.write(buffer, 0 , length);
+                readBytes += length;
+            }
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }finally{
+            try{
+                if(buffIns != null){
+                    buffIns.close();
+                }
+                if(rdf != null){
+                    rdf.close();
+                }
+            }catch(Exception w){
+                logger.error("close error ", w);
+            }
+            
+        }
+        return readBytes;
     }
 }
